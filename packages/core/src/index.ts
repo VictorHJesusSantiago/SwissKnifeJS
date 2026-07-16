58e37311152569a64610ffcef8d47853fc7b2cd4
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-07-16 19:33:51 -0300

    feat: export additional modules for enhanced functionality

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,7 @@
 export * from "./args.js";
 export * from "./http.js";
 export * from "./io.js";
+export * from "./output.js";
+export * from "./watch.js";
+export * from "./plugins.js";
+export * from "./health.js";
