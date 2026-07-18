74197666e9333dc66a1b2a980fa84101ed14f35a
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-07-18 13:19:09 -0300

    feat: add synchronization, import/export, and history management functionalities to snippet manager

--- packages/snippet-manager/src/preload.ts
+++ packages/snippet-manager/src/preload.ts
@@ -3,5 +3,17 @@ contextBridge.exposeInMainWorld("snippets", {
   list: () => ipcRenderer.invoke("snippets:list"),
   save: (value: unknown) => ipcRenderer.invoke("snippets:save", value),
   remove: (id: string) => ipcRenderer.invoke("snippets:remove", id),
-  copy: (code: string) => ipcRenderer.invoke("snippets:copy", code)
+  copy: (code: string) => ipcRenderer.invoke("snippets:copy", code),
+  search: (options: unknown) => ipcRenderer.invoke("snippets:search", options),
+  tags: () => ipcRenderer.invoke("snippets:tags"),
+  categories: () => ipcRenderer.invoke("snippets:categories"),
+  syncGetPath: () => ipcRenderer.invoke("sync:getPath"),
+  syncChoosePath: () => ipcRenderer.invoke("sync:choosePath"),
+  syncSetPath: (path: string | undefined) => ipcRenderer.invoke("sync:setPath", path),
+  syncNow: () => ipcRenderer.invoke("sync:now"),
+  exportSnippets: (format: "json" | "yaml") => ipcRenderer.invoke("io:export", format),
+  importSnippets: () => ipcRenderer.invoke("io:import"),
+  historyList: (snippetId: string) => ipcRenderer.invoke("history:list", snippetId),
+  historyDiff: (versionId: string, snippetId: string) => ipcRenderer.invoke("history:diff", versionId, snippetId),
+  historyRestore: (versionId: string) => ipcRenderer.invoke("history:restore", versionId)
 });
